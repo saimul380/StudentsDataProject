24277d98b22bb90a2a31c11c3d81be94db0a4a13
saimul380 <c223173@ugrad.iiuc.ac.bd>
2023-09-20 00:40:34 +0600

    Add project files.

---
+++ StudentsDataProject/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace StudentsDataProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MatrixidBox = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.sessionBox = new System.Windows.Forms.TextBox();
+            this.cgpaBox = new System.Windows.Forms.TextBox();
+            this.Submit = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Search = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Cyan;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(152, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matrix ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Cyan;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Cyan;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(163, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Session";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(183, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CGPA";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // MatrixidBox
+            // 
+            this.MatrixidBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatrixidBox.Location = new System.Drawing.Point(315, 44);
+            this.MatrixidBox.Name = "MatrixidBox";
+            this.MatrixidBox.Size = new System.Drawing.Size(204, 38);
+            this.MatrixidBox.TabIndex = 4;
+            this.MatrixidBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.Location = new System.Drawing.Point(315, 93);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(204, 38);
+            this.NameBox.TabIndex = 5;
+            this.NameBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // sessionBox
+            // 
+            this.sessionBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sessionBox.Location = new System.Drawing.Point(315, 147);
+            this.sessionBox.Name = "sessionBox";
+            this.sessionBox.Size = new System.Drawing.Size(204, 38);
+            this.sessionBox.TabIndex = 6;
+            this.sessionBox.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // cgpaBox
+            // 
+            this.cgpaBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cgpaBox.Location = new System.Drawing.Point(315, 203);
+            this.cgpaBox.Name = "cgpaBox";
+            this.cgpaBox.Size = new System.Drawing.Size(204, 38);
+            this.cgpaBox.TabIndex = 7;
+            this.cgpaBox.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Submit
+            // 
+            this.Submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Submit.Location = new System.Drawing.Point(51, 299);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(115, 38);
+            this.Submit.TabIndex = 8;
+            this.Submit.Text = "Submit";
+            this.Submit.UseVisualStyleBackColor = false;
+            this.Submit.Click += new System.EventHandler(this.Submit_Click);
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Update.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update.Location = new System.Drawing.Point(189, 299);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(117, 38);
+            this.Update.TabIndex = 9;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Search
+            // 
+            this.Search.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search.Location = new System.Drawing.Point(334, 299);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(116, 38);
+            this.Search.TabIndex = 10;
+            this.Search.Text = "Search";
+            this.Search.UseVisualStyleBackColor = false;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(477, 299);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(109, 38);
+            this.Delete.TabIndex = 11;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = false;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(169, 378);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(386, 159);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(1065, 580);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.cgpaBox);
+            this.Controls.Add(this.sessionBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.MatrixidBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MatrixidBox;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox sessionBox;
+        private System.Windows.Forms.TextBox cgpaBox;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ StudentsDataProject/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentsDataProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        //submit
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=LAPTOP-Q7KTNQDN\\SQLEXPRESS;Initial Catalog=UniversityDatabase;Integrated Security=True";
+                SqlConnection sqlConnecton = new SqlConnection(connectionString);
+                sqlConnecton.Open();
+
+                var insertQuery = "insert into student2 values(@matrixID, @name, @session ,@cgpa) ";
+                SqlCommand sqlCommand = new SqlCommand(insertQuery, sqlConnecton);
+                sqlCommand.Parameters.AddWithValue("@matrixID", MatrixidBox.Text);
+                sqlCommand.Parameters.AddWithValue("@name", NameBox.Text);
+                sqlCommand.Parameters.AddWithValue("@session", sessionBox.Text);
+                sqlCommand.Parameters.AddWithValue("@cgpa", double.Parse(cgpaBox.Text));
+
+                sqlCommand.ExecuteNonQuery();
+                sqlConnecton.Close();
+
+                MessageBox.Show("Data Inserted Successfully!!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Data Inserted Wrong!");
+
+            }
+        }
+        //update
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=LAPTOP-Q7KTNQDN\\SQLEXPRESS;Initial Catalog=UniversityDatabase;Integrated Security=True";
+                SqlConnection sqlConnecton = new SqlConnection(connectionString);
+                sqlConnecton.Open();
+
+                string updateCommand = "update student2 set name=@name, session=@session, cgpa=@cgpa where matrixID=@matrixID";
+                SqlCommand sqlCommand = new SqlCommand(updateCommand, sqlConnecton);
+                sqlCommand.Parameters.AddWithValue("@matrixID", MatrixidBox.Text);
+                sqlCommand.Parameters.AddWithValue("@name", NameBox.Text);
+                sqlCommand.Parameters.AddWithValue("@session", sessionBox.Text);
+                sqlCommand.Parameters.AddWithValue("@cgpa", double.Parse(cgpaBox.Text));
+                
+                sqlCommand.ExecuteNonQuery();
+                sqlConnecton.Close();
+
+                MessageBox.Show("Data Updated Successfully!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something Error in Data Update.");
+            }
+        }
+
+        private void Search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=LAPTOP-Q7KTNQDN\\SQLEXPRESS;Initial Catalog=UniversityDatabase;Integrated Security=True";
+                SqlConnection sqlConnecton = new SqlConnection(connectionString);
+                sqlConnecton.Open();
+                if (MatrixidBox.Text.ToString() == "")
+                {
+                    string readCommand = "read * from student2 ";
+                    SqlCommand sqlCommand = new SqlCommand(readCommand, sqlConnecton);
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                    DataTable dataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+                else
+                {
+                    string readCommand = "read * from student2 where matrixID= @matrixID ";
+                    SqlCommand sqlCommand = new SqlCommand(readCommand, sqlConnecton);
+
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                    DataTable dataTable = new DataTable();
+
+                    sqlDataAdapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something Error in Data Search");
+            }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var connectionString = "Data Source=LAPTOP-Q7KTNQDN\\SQLEXPRESS;Initial Catalog=UniversityDatabase;Integrated Security=True";
+                SqlConnection sqlConnecton = new SqlConnection(connectionString);
+                sqlConnecton.Open();
+
+                string deleteCommand = "delete student2 where matrixID= @matrixID";
+                SqlCommand sqlCommand = new SqlCommand(deleteCommand, sqlConnecton);
+                sqlCommand.Parameters.AddWithValue("@matrixID", MatrixidBox.Text);
+               
+
+                sqlCommand.ExecuteNonQuery();
+                sqlConnecton.Close();
+
+                MessageBox.Show("Data Deleted Successfully!!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something Error in Data Delete");
+            }
+        }
+    }
+}
